a2d9863140dfe9fc7f7305f14e746416ee2d3718
csychenci <azuuuuuu@AzuuuuuudeMacBook-Pro.local>
2025-08-22 18:07:30 +0800

    feat: update l.svg.tile

--- src/Leaflet.Renderer.SVG.Tile.js
+++ src/Leaflet.Renderer.SVG.Tile.js
@@ -11,21 +11,28 @@ L.SVG.Tile = L.SVG.extend({
 		this._container.setAttribute('width', this._size.x);
 		this._container.setAttribute('height', this._size.y);
 		this._container.setAttribute('viewBox', [0, 0, this._size.x, this._size.y].join(' '));
-
+		if (options.interactive) {
+			this._container.style.pointerEvents = 'auto';
+		}
 		this._layers = {};
+		if (options.style) {
+			for (let styleName in options.style) {
+				this._container.style[styleName] = options.style[styleName];
+			}
+		}
 	},
 
-	getCoord: function() {
+	getCoord: function () {
 		return this._tileCoord;
 	},
 
-	getContainer: function() {
+	getContainer: function () {
 		return this._container;
 	},
 
 	onAdd: L.Util.falseFn,
 
-	addTo: function(map) {
+	addTo: function (map) {
 		this._map = map;
 		if (this.options.interactive) {
 			for (var i in this._layers) {
@@ -35,6 +42,52 @@ L.SVG.Tile = L.SVG.extend({
 				this._map._targets[L.stamp(layer._path)] = layer;
 			}
 		}
+	},
+	isTextOverlapping(svgElement, tempText, buffer, type) {
+		// 将临时文本元素添加到SVG中，但不显示在屏幕上
+		svgElement.appendChild(tempText);
+
+		// 获取临时文本元素的边界框
+		var tempTextBBox = tempText.getBBox();
+
+		// 遍历SVG中的所有文本元素，检查是否与临时文本元素有重叠
+		var textElements = svgElement.getElementsByClassName(type);
+		for (var i = 0; i < textElements.length; i++) {
+			var textElement = textElements[i];
+			if (textElement === tempText) {
+				continue; // 跳过与自身比较
+			}
+
+			// 获取当前文本元素的边界框
+			var svgBBox = svgElement.getBBox()
+			var textBBox = textElement.getBBox();
+			textBBox.width += buffer
+			textBBox.height += buffer
+			// 检查两个文本元素的边界框是否有重叠
+			if ((tempTextBBox.x < textBBox.x + textBBox.width &&
+				tempTextBBox.x + tempTextBBox.width > textBBox.x &&
+				tempTextBBox.y < textBBox.y + textBBox.height &&
+				tempTextBBox.y + tempTextBBox.height > textBBox.y)
+				|| (tempTextBBox.x + tempTextBBox.width > svgBBox.x + svgBBox.width || tempTextBBox.y + tempTextBBox.height > svgBBox.y + svgBBox.height
+					|| tempTextBBox.x < 0 || tempTextBBox.y < 0)
+			) {
+				// 有重叠，返回true
+				tempText.setAttribute('visibility', 'hidden');
+				if (tempText.textContent) {
+					if (tempText.textContent > textElement.textContent) {
+						svgElement.removeChild(textElement);
+					} else {
+						svgElement.removeChild(tempText);
+					}
+				}
+
+				return true;
+			}
+		}
+
+		// 没有重叠，返回false
+		svgElement.removeChild(tempText);
+		return false;
 	},
 
 	removeFrom: function (map) {
@@ -47,9 +100,9 @@ L.SVG.Tile = L.SVG.extend({
 		delete this._map;
 	},
 
-	_initContainer: function() {
+	_initContainer: function () {
 		L.SVG.prototype._initContainer.call(this);
-		var rect =  L.SVG.create('rect');
+		var rect = L.SVG.create('rect');
 	},
 
 	/// TODO: Modify _initPath to include an extra parameter, a group name
@@ -61,23 +114,56 @@ L.SVG.Tile = L.SVG.extend({
 	},
 
 	_updateIcon: function (layer) {
-		var path = layer._path = L.SVG.create('image'),
-		    icon = layer.options.icon,
-		    options = icon.options,
-		    size = L.point(options.iconSize),
-		    anchor = options.iconAnchor ||
-		        	 size && size.divideBy(2, true),
-		    p = layer._point.subtract(anchor);
-		path.setAttribute('x', p.x);
-		path.setAttribute('y', p.y);
-		path.setAttribute('width', size.x + 'px');
-		path.setAttribute('height', size.y + 'px');
-		path.setAttribute('href', options.iconUrl);
+		const icon = layer.options.icon,
+			iconOptions = icon.options,
+			point = L.point(iconOptions.iconSize),
+			anchor = iconOptions.iconAnchor || point && point.divideBy(2, true),
+			subtract = layer._point.subtract(anchor)
+		if (iconOptions.iconUrl) {
+			const path = layer._path = L.SVG.create('image');
+			path.classList.add(iconOptions?.className);
+			path.setAttribute('x', subtract.x);
+			path.setAttribute('y', subtract.y);
+			path.setAttribute('width', point.x + 'px');
+			path.setAttribute('height', point.y + 'px');
+			path.setAttribute('href', iconOptions.iconUrl);
+			if (iconOptions.rotationAngle) {
+				path.style.transform = "rotate(" + iconOptions.rotationAngle + "deg)"
+				path.style.transformOrigin = (subtract.x + point.x / 2) + "px " + (subtract.y + point.y / 2) + "px"
+			}
+			if (iconOptions.className && !!iconOptions.overlap) {
+				this.isTextOverlapping(this._container, path, 5, iconOptions.className);
+			}
+		} else if (iconOptions.text) {
+			const path = layer._path = L.SVG.create("text")
+			path.classList.add(iconOptions?.className);
+			path.setAttribute('x', subtract.x);
+			path.setAttribute('y', subtract.y);
+			path.textContent = iconOptions.text;
+			path.setAttribute('font-family', 'Arial');
+			path.setAttribute('font-size', '13');
+			path.setAttribute('fill', 'black');
+			if (iconOptions.className && !!iconOptions.overlap) {
+				this.isTextOverlapping(this._container, path, 30, iconOptions.className);
+			}
+		}
+		// var path = layer._path = L.SVG.create('image'),
+		// 	icon = layer.options.icon,
+		// 	options = icon.options,
+		// 	size = L.point(options.iconSize),
+		// 	anchor = options.iconAnchor ||
+		// 		size && size.divideBy(2, true),
+		// 	p = layer._point.subtract(anchor);
+		// path.setAttribute('x', p.x);
+		// path.setAttribute('y', p.y);
+		// path.setAttribute('width', size.x + 'px');
+		// path.setAttribute('height', size.y + 'px');
+		// path.setAttribute('href', options.iconUrl);
 	}
 });
 
 
-L.svg.tile = function(tileCoord, tileSize, opts){
+L.svg.tile = function (tileCoord, tileSize, opts) {
 	return new L.SVG.Tile(tileCoord, tileSize, opts);
 }
 
